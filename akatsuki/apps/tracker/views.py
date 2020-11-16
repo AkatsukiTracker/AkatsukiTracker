aac67c35065801f0090bd14c75fa93c19c8dd3bf
LaTortugaR <vicente.figueroa@sansano.usm.cl>
2020-11-16 15:28:38 -0300

    Arregladas urls y static

--- akatsuki/apps/tracker/views.py
+++ akatsuki/apps/tracker/views.py
@@ -3,7 +3,3 @@ from django.views.generic import TemplateView
 class TrackerView(TemplateView):
 
     template_name = "tracker.html"
-
-class HomeView(TemplateView):
-
-    template_name = "home.html"
