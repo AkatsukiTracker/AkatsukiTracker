efcfc3e463e863733a889e3090d7b7194547bcee
juanfraaa2 <39860136+juanfraaa2@users.noreply.github.com>
2020-11-30 12:59:54 -0300

    Bosquejos de contacto y about.
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- akatsuki/apps/others/urls.py
+++ akatsuki/apps/others/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, AboutView
+from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view()),
     path('about/', AboutView.as_view()),
+    path('contacto/', ContactoView.as_view()),
 ]
--- akatsuki/apps/others/views.py
+++ akatsuki/apps/others/views.py
@@ -2,6 +2,9 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home/index.html"
-    
+
 class AboutView(TemplateView):
     template_name = "home/about.html"
+
+class ContactoView(TemplateView):
+    template_name = "home/contacto.html"
