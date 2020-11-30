e9a641e2531a6c61e1af791fb93c0511442e87ac
LaTortugaR <vicente.figueroa@sansano.usm.cl>
2020-11-30 13:18:37 -0300

    urls

--- akatsuki/apps/others/views.py
+++ akatsuki/apps/others/views.py
@@ -1,7 +1,32 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.contrib.auth.models import User
 
-class HomeView(TemplateView):
-    template_name = "home/index.html"
+def home_view(request):
+    if request.method == 'GET':
+        if request.user.username != "":
+          user = User.objects.filter(username=request.user.username)[0]
+        else:
+            user = False
+        args = {"user": user}
+        return render(request, "home/index.html", args)
+    return render(request, "home/index.html")
 
-class AboutView(TemplateView):
-    template_name = "home/about.html"
+def about_view(request):
+    if request.method == 'GET':
+        if request.user.username != "":
+          user = User.objects.filter(username=request.user.username)[0]
+        else:
+            user = False
+        args = {"user": user}
+        return render(request, "about/index.html", args)
+    return render(request, "about/index.html")
+
+def contacto_view(request):
+    if request.method == 'GET':
+        if request.user.username != "":
+          user = User.objects.filter(username=request.user.username)[0]
+        else:
+            user = False
+        args = {"user": user}
+        return render(request, "contacto/index.html", args)
+    return render(request, "contacto/index.html")    
--- akatsuki/apps/tracker/models.py
+++ akatsuki/apps/tracker/models.py
@@ -9,7 +9,7 @@ class Tienda(models.Model):
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
-    link = models.URLField(unique=True)
+    link = models.URLField(max_length=65000, unique=True)
     tienda = models.ForeignKey(Tienda, on_delete=models.CASCADE)
     img_link = models.URLField(null=True)
 
@@ -21,7 +21,7 @@ class Historial(models.Model):
     tipo = models.CharField(max_length=50)
     fecha = models.DateTimeField(auto_now_add=True)
     precio = models.IntegerField()
-    bs4path = models.CharField(max_length=200, null=True)
+    bs4path = models.CharField(max_length=300, null=True)
 
     class Disponibilidad(models.IntegerChoices):
         NO = 0, _('No')
