28b8fe569ea391974d778510f6b1baceba7cd3ad
Vicente Lineros Pardo <vlinerospardo@gmail.com>
2021-01-10 23:56:38 -0300

    akatusukiii

--- akatsuki/apps/others/urls.py
+++ akatsuki/apps/others/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', home_view, name="home"),
     path('about/', about_view, name="about"),
-    path('contacto/', contacto_view, name="contacto"),
+    path('contribuye/', contacto_view, name="contacto"),
 ]
