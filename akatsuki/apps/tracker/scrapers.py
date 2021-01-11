202643e793232f0657dbbd1801fb738f83de4afb
Vicente Figueroa <68250848+LaTortugaR@users.noreply.github.com>
2021-01-11 20:56:40 -0300

    steam v2

--- akatsuki/apps/tracker/scrapers.py
+++ akatsuki/apps/tracker/scrapers.py
@@ -958,11 +958,18 @@ class SteamScraper(BaseScraper):
             except:
                 if ruta == "div.discount_final_price":
                     self.status = 2
-                elif ruta == "div.game_purchase_price.price":
+                else:
+                    ruta = "div.game_purchase_price.price"
+                    self.path = "div.game_purchase_price.price/0"
+                    self.status = 1
+                    self.precio = string_to_number(soup.select(ruta)[int(indice)].text.strip().split('\n')[0])               
+            else:
+                if ruta == "div.game_purchase_price.price":
+                    ruta = "div.discount_original_price"
+                    self.path = "div.discount_original_price/0"
+                    self.status = 1
                     self.precio = string_to_number(soup.select(ruta)[int(indice)].text.strip().split('\n')[0])
-                elif ruta == "div.discount_original_price":
-                    self.status == 1
-                    self.path = "div.game_purchase_price.price" + "/0"
+                else:
                     self.precio = string_to_number(soup.select(ruta)[int(indice)].text.strip().split('\n')[0])
 
             
